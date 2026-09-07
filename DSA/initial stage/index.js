4d7ab4dc45b88cafc0c4a47bf979e27bd35d16b6
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-09-07 22:51:11 +0600

    dsa

--- DSA/initial stage/index.js
+++ DSA/initial stage/index.js
@@ -1,29 +1,13 @@
-//Two Sum II - Input Array Is Sorted
+//----Remove Duplicates from Sorted List--//
+let removeDuplicates = function (numbers) {
+  let slow = 0;
 
-let inputArr = [2, 7, 11, 15];
-let target = 9;
-
-let twoSum = function (numbers, target) {
-  let left = 0;
-  let right = numbers.length - 1;
-  while (left < right) {
-    let sum = numbers[left] + numbers[right];
-    if (sum === target) {
-      return [left + 1, right + 1]; // Return 1-based indices
-    } else if (sum < target) {
-      left++;
-    } else {
-      right--;
+  for (let fast = 1; fast < numbers.length; fast++) {
+    if (numbers[fast] !== numbers[slow]) {
+      slow++;
+      numbers[slow] = numbers[fast];
     }
   }
-  return []; // Return an empty array if no solution is found
+  return slow + 1;
 };
 
-let result = twoSum(inputArr, target);
-console.log(result); // Output: [1, 2]
-
-//----Remove Duplicates from Sorted List--//
-
-let removeDuplicates = function (head) {
-  if (!head) return head;
-};
