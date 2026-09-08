23a36c9a82f4da8c58a725e63cf7f807165f1dc8
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-09-08 20:41:51 +0600

    crud operations

--- Express/index.js
+++ Express/index.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+let users = [
+  { id: 1, name: "John", age: 30 },
+  { id: 2, name: "Smith", age: 25 },
+  { id: 3, name: "Shawn", age: 25 },
+  { id: 4, name: "Michael", age: 28 },
+];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+app.get("/users/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find((user) => user.id === userId);
+  if (!user) {
+    return res.status(404).json({ message: "user not found" });
+  }
+  res.json(user);
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
