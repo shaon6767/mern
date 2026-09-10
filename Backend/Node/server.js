aa8a9707e984b767c20dd229ce10534632fc1698
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-09-10 22:13:12 +0600

    nav using node

--- Backend/Node/server.js
+++ Backend/Node/server.js
@@ -1,12 +1,41 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Node</title></head>");
-  res.write("<body><h1>Hello from Node</h1></body>");
-  res.write("</html");
+  console.log(req.url, req.method);
+  if (req.url === "/home") {
+    res.write("<h1>Welcome to the Home Page</h1>");
+    return res.end();
+  } else if (req.url === "/men") {
+    res.write("<h1>Welcome to the Men's Page</h1>");
+    return res.end();
+  } else if (req.url === "/women") {
+    res.write("<h1>Welcome to the Women's Page</h1>");
+    return res.end();
+  } else if (req.url === "/kids") {
+    res.write("<h1>Welcome to the Kids' Page</h1>");
+    return res.end();
+  } else if (req.url === "/cart") {
+    res.write("<h1>Welcome to the Cart Page</h1>");
+    return res.end();
+  }
+  res.write(
+    `<html>
+      <head>
+        <title>Page</title>
+      </head>
+      <body>
+        <nav>
+          <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/men">Men</a></li>
+            <li><a href="/women">Women</a></li>
+            <li><a href="/kids">Kids</a></li>
+            <li><a href="/cart">Cart</a></li>
+          </ul>
+        </nav>
+      </body>
+    </html>`,
+  );
   res.end();
-  console.log(req);
 });
 
 const PORT = 3000;
