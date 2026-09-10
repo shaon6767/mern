05317f23283af88a22a717b24d31313970b58b35
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-09-10 15:54:47 +0600

    first response in node

--- Backend/Node/server.js
+++ Backend/Node/server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Node</title></head>");
+  res.write("<body><h1>Hello from Node</h1></body>");
+  res.write("</html");
+  res.end();
   console.log(req);
 });
 
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`Server is running on http//:localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
