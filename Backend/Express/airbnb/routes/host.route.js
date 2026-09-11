cd8c4a571825fc58dfb877cbbbae8690443def8b
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-09-12 01:16:30 +0600

    added html in views

--- Backend/Express/airbnb/routes/host.route.js
+++ Backend/Express/airbnb/routes/host.route.js
@@ -1,24 +1,15 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 router.get("/add-home", (req, res) => {
-  res.send(`
-    <h1>Register here</h1>
-    <form action="/host/add-home" method="POST">
-      <label for="name">Name:</label>
-      <input type="text" id="name" name="name" required><br><br>
-      <input type="submit" value="Submit">
-      </form>
-    `);
+  res.sendFile(path.join(__dirname, "../", "views", "register.html"));
 });
 
 router.post("/add-home", (req, res) => {
   const { name } = req.body;
   console.log("Received data:", name);
-  res.send(`
-    <h1>Thank you for registering, ${name}!</h1>
-    <a href ="/">Goto Home </a>
-    `);
+  res.sendFile(path.join(__dirname, "../", "views", "addedHome.html"));
 });
 
 module.exports = router;
--- Backend/Express/airbnb/server.js
+++ Backend/Express/airbnb/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const userRouter = require("./routes/user.route");
 const hostRouter = require("./routes/host.route");
 const app = express();
@@ -9,9 +10,9 @@ app.use(userRouter);
 app.use("/host", hostRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send(`Error 404: Page not found <br/>
-    <a href="/">Home</a>`);
+  res.sendFile(path.join(__dirname, "./", "views", "err404.html"));
 });
+
 const port = 3003;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
